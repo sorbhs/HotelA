10405b32a03bd4711cabdaafab281391e630ad43
Sourabh sharma <mesourabhsharma@gmail.com>
2016-12-02 19:09:47 +0530

    flyway-39

--- src/com/mindtree/controller/HotelController.java
+++ src/com/mindtree/controller/HotelController.java
@@ -38,7 +38,7 @@ import com.mindtree.service.HotelService;
  *docker
  *docker upload
  *docker-push-5
- *flyway-38
+ *flyway-39
  */
 @Controller
 public class HotelController{
---
+++ src/main/resources/db/migration/V5__Create_hotel_tables.sql
@@ -0,0 +1,1 @@
+insert into user values(4,"sourabh@whatsapp.com","whatsapp");
