f73bb3d535cab2f57520d68e42b5f29feba01f57
sourabh sharma <mesourabhsharma@gmail.com>
2017-02-01 14:46:28 +0530

    edited for scm TEST in github itself
    edited for scm TEST in github itself

--- src/com/mindtree/controller/HotelController.java
+++ src/com/mindtree/controller/HotelController.java
@@ -43,7 +43,7 @@ public class HotelController{
 	 */
 	Hotel selectedHotel;
 	/**
-	 * 
+	 * edited for scm TEST in github itself
 	 */
 	Users selectedUser;
 	// HotelService service=new HotelService();
